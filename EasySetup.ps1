7794fabec58d4e37f78a522a06587b137a701e45
Scott Gillis <gillissm@users.noreply.github.com>
2018-04-16 13:40:20 -0400

    Create EasySetup.ps1

---
+++ EasySetup.ps1
@@ -0,0 +1,28 @@
+$ModuleFolderName = 'TheCodeAttic.Helix.PowerShellProjectCreator'
+$ModulePath =($env:PSModulePath).Split(";")[0] + "\$ModuleFolderName"
+
+if(-NOT (Test-path -Path $ModulePath))
+{
+   Write-Host "$ModuleFolderName does not exist, so will be created at $ModulePath" -ForegroundColor 'Green'
+   New-Item -Path ($env:PSModulePath).Split(";")[0] -ItemType Directory -Name $ModuleFolderName
+}
+
+Write-Host "Download and save $ModuleFolderName" -ForegroundColor 'Green'
+(Invoke-WebRequest -UseBasicParsing -Uri https://raw.githubusercontent.com/gillissm/TheCodeAttic.Helix.PowerShellProjectCreator/master/HelixProjectCreator.psm1).Content | Out-File "$ModulePath\$ModuleName.psm1" -Force
+
+Write-Host "Update and create the NuGet profile"
+$profilePath = Split-Path $profile
+$NuGetProfilePath = Join-Path $profile "NuGet_profile.ps1"
+$ImportModuleCommand = "Import-Module $ModuleFolderName"
+
+
+if(-NOT (Test-path -Path $NuGetProfilePath))
+{
+   Write-Host "NuGet_profile.ps1 does not exist, so will be created at $NuGetProfilePath" -ForegroundColor 'Green'
+   $ImportModuleCommand | Out-File $NuGetProfilePath
+}
+else
+{
+  Write-Host "NuGet_profile.ps1 does exist, updating with Import-Module command" -ForegroundColor 'Green'
+  Add-Content -Path $NuGetProfilePath -value $ImportModuleCommand
+}
